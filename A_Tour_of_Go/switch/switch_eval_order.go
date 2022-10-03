44f1d216f60155828524dbcfc2b7cbcb78796fa5
jin-nakajima <jinius.nakajima180405@gmail.com>
2022-10-03 17:35:36 +0900

    switch_eval_order

--- A_Tour_of_Go/switch/switch_eval_order.go
+++ A_Tour_of_Go/switch/switch_eval_order.go
@@ -8,7 +8,9 @@ import (
 func main() {
 	fmt.Println("Whens Saturday?")
 	today := time.Now().Weekday()
-	fmt.Println("Today is ", today)
+	fmt.Println("Today is ", today) // youbi
+	fmt.Println(time.Saturday)      // Saturday
+	fmt.Println(today + 3)          // Thursday
 	switch time.Saturday {
 	case today + 0:
 		fmt.Println("Today.")
